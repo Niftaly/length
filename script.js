f4ff80d83bb55355e05ba977eee9a8890a45320e
Niftaly <108418135+Niftaly@users.noreply.github.com>
2022-11-23 17:57:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let data = {
+    "object": [{
+        "id": "1",
+        "title": "Salam",
+        "sample": "Buyurun",
+        "question": "Netersen?"
+    },
+    {
+        "id": "2",
+        "title": "Salam",
+        "sample": "Buyurun",
+        "question": "Netersen?"
+    },
+    {
+        "id": "3",
+        "title": "Salam",
+        "sample": "Buyurun",
+        "question": "Netersen?"
+    },
+    {
+        "id": "4",
+        "title": "Salam",
+        "sample": "Buyurun",
+        "question": "Netersen?"
+    }
+    ]};
+
+    let objectLength = Object.keys(data.object[0]).length;
+    console.log(objectLength);
